a467e59aa0597fad55e7a9e545c5fede901a915d
hussein <man_hsy@hotmail.com>
2013-08-01 11:51:04 -0700

    created a rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+IslamBook::Application.config.secret_key_base = '55bb67125cb613d6b88b25ee00922f83f2b81a7c58d1c81c83fb91b1cdab67f4e512e74774a468a2e82cb6073688e8a4702c45e21a9fa4031cd229f1c9183f9c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+IslamBook::Application.config.session_store :cookie_store, key: '_islamBook_session'
